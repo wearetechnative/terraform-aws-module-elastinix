b4b7d82cb3bd5022cb4721e49f0bb1b12191cf92
Pim Snel <post@pimsnel.com>
2025-09-18 19:10:29 +0200

    improve known hosts stuff

--- instance/script/local_exec_upload_ssh_workloads_key.sh
+++ instance/script/local_exec_upload_ssh_workloads_key.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-
 echo
 echo "UPDATE KNOWN HOSTS"
 ssh-keygen -R $(echo $TARGET | sed "s/root@//")
+ssh-keyscan -q $(echo $TARGET | sed "s/root@//") >> ~/.ssh/known_hosts
 
-export AWS_PROFILE=$($SCRIPT_PATH/find_profile.sh $AWS_ACCOUNT_ID)
+AWS_PROFILE=$($SCRIPT_PATH/find_profile.sh $AWS_ACCOUNT_ID)
+eval SSH_ID_FILE_ABS=$SSH_ID_FILE
 
 # TODO only if debug=true
 echo $AWS_PROFILE >> /tmp/debug-target.txt
@@ -18,8 +19,9 @@ CURR_DIR=$(pwd)
 echo $CURR_DIR
 
 cd secrets
-SYS_SSH_KEY=$(agenix -d system_sshd_key.age --identity $SSH_ID_FILE)
+
+SYS_SSH_KEY=$(agenix -d system_sshd_key.age --identity $SSH_ID_FILE_ABS)
 cd $CURR_DIR
 
-echo $SYS_SSH_KEY | ssh -F $SSH_CONFIG_FILE -oStrictHostKeyChecking=no -i $SSH_ID_FILE \
+echo "$SYS_SSH_KEY" | ssh -F $SSH_CONFIG_FILE -oStrictHostKeyChecking=no -i $SSH_ID_FILE_ABS \
   $TARGET 'cat - > /tmp/system_sshd_key && chmod 600 /tmp/system_sshd_key && chown root:root /tmp/system_sshd_key'
